dada52efc0ee784c2ea120b55f9eb48f5b083a78
aharon13 <aharon.yedigaryan@gmail.com>
2017-09-04 17:46:09 +0400

    Add template

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,32 @@
+<nav class="navbar-default navbar-static-side" role="navigation">
+    <div class="sidebar-collapse">
+        <ul class="nav metismenu" id="side-menu">
+            <li class="nav-header">
+                <div class="dropdown profile-element">
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                        <span class="clear">
+                            <span class="block m-t-xs">
+                                <strong class="font-bold">
+                                    @if (Auth::guest())
+                                        <a href="{{ route('login') }}">Login</a>
+                                    @else
+                                        {{ Auth::user()->name }}
+                                    @endif
+                                </strong>
+                        </span>
+                    </a>
+                </div>
+                <div class="logo-element">
+                    CH
+                </div>
+            </li>
+            <li class="{{ isActiveRoute('main') }}">
+                <a href="{{ url('/') }}"><i class="fa fa-th-large"></i> <span class="nav-label">Main view</span></a>
+            </li>
+            <li class="{{ isActiveRoute('minor') }}">
+                <a href="{{ url('/minor') }}"><i class="fa fa-th-large"></i> <span class="nav-label">Minor view</span> </a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
