d7b4fe2c06c166f9c21101ee03576f365e9c82b6
aharon13 <aharon.yedigaryan@gmail.com>
2018-03-05 22:52:19 +0400

    fixing

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,19 +22,19 @@
             </li>
 
             <li class="{{ isActiveRoute('welcome') }}">
-                <a href="{{ url('/welcome') }}"><i class="fa fa-chart-pie"></i> <span class="nav-label">My Charts Gallery</span> </a>
+                <a href="{{ url('/welcome') }}"><i class="fa fa-pie-chart"></i> <span class="nav-label">My Charts Gallery</span> </a>
             </li>
             <li class="{{ isActiveRoute('createchart') }}">
-                <a href="{{ url('/createchart') }}"><i class="fa fa-pencil-alt"></i> <span class="nav-label">Create Thing Chart</span> </a>
+                <a href="{{ url('/createchart') }}"><i class="fa fa-pencil"></i> <span class="nav-label">Create Thing Chart</span> </a>
             </li>
             <li class="{{ isActiveRoute('t') }}">
                 <a href="{{ url('/createchart') }}"><i class="fa fa-book"></i> <span class="nav-label">Tutorials</span> </a>
             </li>
             <li class="{{ isActiveRoute('pu') }}">
-                <a href="{{ url('/createchart') }}"><i class="fa fa-images"></i> <span class="nav-label">Public Gallery</span> </a>
+                <a href="{{ url('/createchart') }}"><i class="fa fa-picture-o"></i> <span class="nav-label">Public Gallery</span> </a>
             </li>
             <li class="{{ isActiveRoute('pr') }}">
-                <a href="{{ url('/createchart') }}"><i class="fa fa-dollar-sign"></i> <span class="nav-label">Pricing and Plans</span> </a>
+                <a href="{{ url('/createchart') }}"><i class="fa fa-usd"></i> <span class="nav-label">Pricing and Plans</span> </a>
             </li>
             <li class="{{ isActiveRoute('s') }}">
                 <a href="{{ url('/createchart') }}"><i class="fa fa-cog"></i> <span class="nav-label">Settings</span> </a>
@@ -43,7 +43,7 @@
                 <a href="{{ url('/createchart') }}"><i class="fa fa-user"></i> <span class="nav-label">Profile</span> </a>
             </li>
             <li class="{{ isActiveRoute('l') }}">
-                <a href="{{ url('/createchart') }}"><i class="fa fa-sign-out-alt"></i> <span class="nav-label">LOGOUT</span> </a>
+                <a href="{{ url('/createchart') }}"><i class="fa fa-sign-out"></i> <span class="nav-label">LOGOUT</span> </a>
             </li>
         </ul>
 
