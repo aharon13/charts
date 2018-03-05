d9b2dee294979388f0c0333e2c7a94f5daa13829
aharon13 <aharon.yedigaryan@gmail.com>
2018-03-05 22:40:23 +0400

    remove new font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" href="{!! asset('css/vendor.css') !!}" />
     <link rel="stylesheet" href="{!! asset('css/app.css') !!}" />
     <link rel="stylesheet" href="{!! asset('css/custom_styles.css') !!}" />
-    <link rel="stylesheet" href="{!! asset('css/fontawesome-all.css') !!}" />
-    
+    <!-- <link rel="stylesheet" href="{!! asset('css/fontawesome-all.css') !!}" /> -->
+
     <link href="{!! asset('css/steps/jquery.steps.css') !!}" rel="stylesheet">
     <link rel="stylesheet" href="{!! asset('js/iconpicker/iconpicker.css') !!}" />
 </head>
